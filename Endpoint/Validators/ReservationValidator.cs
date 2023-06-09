1123788536ef4ef767fe4c64e9d74675fb3295c9
Peter Peto <peter.peto@stud.uni-obuda.hu>
2023-06-10 00:21:40 +0200

    Validator: Reservation

---
+++ Endpoint/Validators/ReservationValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using Endpoint.Models.Data;
+using Endpoint.Repositories.GenericRepository;
+using Endpoint.Repositories.Interfaces;
+
+namespace Endpoint.Validators
+{
+    public class ReservationValidator : AbstractValidator<Reservation>
+    {
+        public ReservationValidator(ITableRepository repository)
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(50);
+            RuleFor(x => x.Contact)
+                .NotEmpty();
+            RuleFor(x => x.Begin)
+                .NotEmpty();
+            RuleFor(x => x.End)
+                .NotEmpty()
+                .GreaterThan(x => x.Begin);
+            RuleFor(x => x.TableId)
+                .NotEmpty()
+                .Must(tableId => repository.ReadAll().Any(item => item.Id == tableId))
+                .WithMessage("Table does not exists.");
+            RuleFor(x => x.Person)
+                .GreaterThanOrEqualTo(1);
+        }
+    }
+}
