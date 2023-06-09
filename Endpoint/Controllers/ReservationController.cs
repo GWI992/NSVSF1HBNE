b31d851b3bcd36b0a2417e2fc88d5ff991b744d6
Peter Peto <peter.peto@stud.uni-obuda.hu>
2023-06-10 00:22:39 +0200

    Controller: ReservationController

---
+++ Endpoint/Controllers/ReservationController.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Endpoint.Logic.Interfaces;
+using Endpoint.Models.Data;
+using Endpoint.Controllers.GenericControllers;
+using Endpoint.Logic.Exceptions;
+
+namespace Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ReservationController : GenericBaseController<Reservation, Reservation>
+    {
+        public ReservationController(ILogic<Reservation, Reservation> logic, IValidator<Reservation> validator) : base(logic, validator)
+        {
+        }
+    }
+}
