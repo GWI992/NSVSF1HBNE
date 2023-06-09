cbbadc5552cff04a1fcdf2d17acc334419f9f5e0
Peter Peto <peter.peto@stud.uni-obuda.hu>
2023-06-09 19:09:09 +0200

    Controller: TableController

---
+++ Endpoint/Controllers/TableController.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Endpoint.Logic.Interfaces;
+using Endpoint.Models.Data;
+using Endpoint.Controllers.GenericControllers;
+
+namespace Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TableController : GenericBaseController<Table, Table>
+    {
+        public TableController(ILogic<Table, Table> logic, IValidator<Table> validator) : base(logic, validator)
+        {
+        }
+    }
+}
