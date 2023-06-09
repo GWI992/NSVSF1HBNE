1b0f06c8bf8e1919b1209f0476252b632f39e633
Peter Peto <peter.peto@stud.uni-obuda.hu>
2023-06-10 00:22:52 +0200

    Migration: Reservations

--- Endpoint/Migrations/20230609221444_Reservations.Designer.cs
+++ Endpoint/Migrations/20230609221444_Reservations.Designer.cs
@@ -4,6 +4,7 @@ using Endpoint.Repositories.Databases;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Endpoint.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230609221444_Reservations")]
+    partial class Reservations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
