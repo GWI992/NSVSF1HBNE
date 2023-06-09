27fe0ceb56da5a94e2edce8d79c5191ecb62b4d6
Peter Peto <peter.peto@stud.uni-obuda.hu>
2023-06-09 22:10:58 +0200

    Validator: Capacity rule for TableValidator

--- Endpoint/Validators/TableValidator.cs
+++ Endpoint/Validators/TableValidator.cs
@@ -10,6 +10,9 @@ namespace Endpoint.Validators
             RuleFor(x => x.Name)
                 .NotEmpty()
                 .MaximumLength(50);
+            RuleFor(x => x.Capacity)
+                .NotEmpty()
+                .InclusiveBetween(1, 4);
         }
     }
 }
