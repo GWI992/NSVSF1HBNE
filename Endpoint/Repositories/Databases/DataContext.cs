c7aeed60f68a939c6267057ad1df17ae1a2301e2
Peter Peto <peter.peto@stud.uni-obuda.hu>
2023-06-10 00:20:53 +0200

    DbContext: Reservation

--- Endpoint/Repositories/Databases/DataContext.cs
+++ Endpoint/Repositories/Databases/DataContext.cs
@@ -10,11 +10,21 @@ namespace Endpoint.Repositories.Databases
     public class DataContext : DbContext
     {
         public DbSet<Table> Tables { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Table>()
+                .HasMany(c => c.Reservations)
+                .WithOne(d => d.Table)
+                .IsRequired();
+            modelBuilder.Entity<Reservation>()
+                .HasOne(d => d.Table)
+                .WithMany(c => c.Reservations)
+                .OnDelete(DeleteBehavior.Restrict)
+                .IsRequired();
             base.OnModelCreating(modelBuilder);
         }
 
