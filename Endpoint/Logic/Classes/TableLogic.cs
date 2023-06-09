25956391f243213810abc932ab72d2313feeeabb
Peter Peto <peter.peto@stud.uni-obuda.hu>
2023-06-09 19:08:01 +0200

    Logic: TableLogic

---
+++ Endpoint/Logic/Classes/TableLogic.cs
@@ -0,0 +1,47 @@
+﻿using Endpoint.Logic.Exceptions;
+using Endpoint.Logic.Interfaces;
+using Endpoint.Models.Data;
+using Endpoint.Repositories.Interfaces;
+
+namespace Endpoint.Logic.Classes
+{
+    public class TableLogic : ILogic<Table, Table>
+    {
+        ITableRepository _repository;
+        public TableLogic(ITableRepository repository)
+        {
+            this._repository = repository;
+        }
+
+        public void Create(Table item)
+        {
+            this._repository.Create(item);
+        }
+
+        public void Delete(string id)
+        {
+            this._repository.Delete(id);
+        }
+
+        public Table Read(string id)
+        {
+            Table table = this._repository.Read(id);
+            if (table == null)
+            {
+                throw new EntityNotFoundException();
+            }
+            return table;
+        }
+
+        public IList<Table> ReadAll()
+        {
+            return this._repository.ReadAll()?.ToList();
+        }
+
+        public void Update(string id, Table item)
+        {
+            this.Read(id); // Check entity is exists
+            this._repository.Update(id, item);
+        }
+    }
+}
