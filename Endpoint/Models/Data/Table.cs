45122c8c90cf2df8551ed63071f5e89a1aa0cff2
Peter Peto <peter.peto@stud.uni-obuda.hu>
2023-06-09 19:05:19 +0200

    Model: Table

---
+++ Endpoint/Models/Data/Table.cs
@@ -0,0 +1,29 @@
+﻿using Endpoint.Models.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Endpoint.Models.Data
+{
+    [Table("tables")]
+    public class Table : IHasId
+    {
+        [Key]
+        [Required]
+        [Column("id")]
+        public string? Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Column("name")]
+        public string Name{ get; set; }
+
+        [Required]
+        [Column("capacity")]
+        public int Capacity { get; set; }
+
+        public Table()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
