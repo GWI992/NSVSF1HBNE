d04153ce48a1dab9b3125989284119905ccdebc5
Peter Peto <peter.peto@stud.uni-obuda.hu>
2023-06-10 00:21:11 +0200

    Repository: Reservation + Interface

---
+++ Endpoint/Repositories/Interfaces/IReservationRepository.cs
@@ -0,0 +1,13 @@
+﻿using Endpoint.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Endpoint.Repositories.Interfaces
+{
+    public interface IReservationRepository : IRepository<Reservation>
+    {
+    }
+}
---
+++ Endpoint/Repositories/ModelRepositories/ReservationRepository.cs
@@ -0,0 +1,19 @@
+﻿using Endpoint.Models.Data;
+using Endpoint.Repositories.Databases;
+using Endpoint.Repositories.GenericRepository;
+using Endpoint.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Endpoint.Repositories.ModelRepositories
+{
+    public class ReservationRepository : Repository<Reservation>, IReservationRepository
+    {
+        public ReservationRepository(DataContext ctx) : base(ctx)
+        {
+        }
+    }
+}
