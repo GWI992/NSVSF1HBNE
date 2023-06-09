0436d57d782be9af33d95409de50c672743478de
Peter Peto <peter.peto@stud.uni-obuda.hu>
2023-06-09 19:06:58 +0200

    Repository: TableRepository + ITableRepository interface

---
+++ Endpoint/Repositories/ModelRepositories/TableRepository.cs
@@ -0,0 +1,19 @@
+﻿using Endpoint.Models.Data;
+using Endpoint.Repositories.Databases;
+using Endpoint.Repositories.GenericRepository;
+using Endpoint.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Endpoint.Repositories.ModelRepositories
+{
+    public class TableRepository : Repository<Table>, ITableRepository
+    {
+        public TableRepository(DataContext ctx) : base(ctx)
+        {
+        }
+    }
+}
