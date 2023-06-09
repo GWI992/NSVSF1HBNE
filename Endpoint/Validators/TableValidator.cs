7be2d910922a605b766cae878a6e4b47ea8a3c82
Peter Peto <peter.peto@stud.uni-obuda.hu>
2023-06-10 00:21:50 +0200

    Reservation: TableValidator update

--- Endpoint/Validators/TableValidator.cs
+++ Endpoint/Validators/TableValidator.cs
@@ -1,15 +1,19 @@
 ﻿using FluentValidation;
 using Endpoint.Models.Data;
+using Endpoint.Repositories.Interfaces;
 
 namespace Endpoint.Validators
 {
     public class TableValidator : AbstractValidator<Table>
     {
-        public TableValidator()
+        public TableValidator(ITableRepository repository)
         {
             RuleFor(x => x.Name)
                 .NotEmpty()
                 .MaximumLength(50);
+            RuleFor(x => x)
+                .Must(x => !repository.ReadAll().Any(item => item.Name == x.Name && item.Id != x.Id && x.Id != null))
+                .WithMessage("Table already exists");
             RuleFor(x => x.Capacity)
                 .NotEmpty()
                 .InclusiveBetween(1, 4);
