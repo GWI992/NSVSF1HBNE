0dd2b51ced3391ffcb29df5205ea1b1cecab2ee7
Peter Peto <peter.peto@stud.uni-obuda.hu>
2023-06-09 19:07:32 +0200

    Logic: ILogic interface

---
+++ Endpoint/Logic/Interfaces/ILogic.cs
@@ -0,0 +1,11 @@
+﻿namespace Endpoint.Logic.Interfaces
+{
+    public interface ILogic<TReq, TRes>
+    {
+        void Create(TReq item);
+        void Delete(string id);
+        TRes Read(string id);
+        IList<TRes> ReadAll();
+        void Update(string id, TReq item);
+    }
+}
