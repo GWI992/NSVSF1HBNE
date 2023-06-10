9f9e0b291d01f5bf7ee3d21158f0b8210dc44f29
Peter Peto <peter.peto@stud.uni-obuda.hu>
2023-06-10 23:19:11 +0200

    Controller: AccountController - Register and Login

---
+++ Endpoint/Controllers/AccountController.cs
@@ -0,0 +1,76 @@
+﻿using Endpoint.Models.Data;
+using Endpoint.Models.Requests;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApiUser> _userManager;
+        private readonly SignInManager<ApiUser> _signInManager;
+
+        public AccountController(UserManager<ApiUser> userManager, SignInManager<ApiUser> signInManager)
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDTO userDTO)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var user = new ApiUser() {
+                    Email = userDTO.Email,
+                    UserName = userDTO.Email,
+                };
+                var result = await _userManager.CreateAsync(user, userDTO.Password);
+                if(!result.Succeeded)
+                {
+                    return BadRequest("$User Registration Attempt Failed:" + result.Errors.First().Description);
+                }
+
+                await _userManager.AddToRoleAsync(user, "MANAGER");
+                return Accepted();
+            }
+            catch(Exception ex)
+            {
+                return Problem($"Something Wnt wrong in the {nameof(Register)}", statusCode: 500);
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO userDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _signInManager.PasswordSignInAsync(userDTO.Email, userDTO.Password, false, false);
+                if(!result.Succeeded)
+                {
+                    return Unauthorized(result.ToString());
+                }
+
+                return Accepted();
+            }
+            catch (Exception ex)
+            {
+                return Problem($"Something Wnt wrong in the {nameof(Login)}", statusCode: 500);
+            }
+        }
+    }
+}
