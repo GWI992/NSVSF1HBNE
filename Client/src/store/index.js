b0c129baca571db7f67724e259f0a1e8fffd3292
Peter Peto <peter.peto@stud.uni-obuda.hu>
2023-06-11 08:37:43 +0200

    Client: Reservation api methods

--- Client/src/store/index.js
+++ Client/src/store/index.js
@@ -1,12 +1,14 @@
 import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
 import auth from "./modules/auth";
+import reservation from "./modules/reservation";
 import table from "./modules/table";
 
 // Create store
 export default new Vuex.Store({
     modules: {
         auth,
+        reservation,
         table,
     },
     plugins: [createPersistedState()],
---
+++ Client/src/store/modules/reservation.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+var promiseFinally = require('promise.prototype.finally');
+promiseFinally.shim();
+
+const actions = {
+    async ReservationList({ commit }) {
+        return new Promise((resolve, reject) => {
+            axios.get("/api/reservation")
+                .then(response => {
+                    resolve(response.data);
+                })
+                .catch(err => {
+                    console.warn(err);
+                    reject(err);
+                });
+        });
+    },
+    async ReservationGet({ commit }, reservationId) {
+        return new Promise((resolve, reject) => {
+            axios.get("/api/reservation/" + reservationId)
+                .then(response => {
+                    resolve(response.data);
+                })
+                .catch(err => {
+                    console.warn(err);
+                    reject(err);
+                });
+        });
+    },
+    async ReservationCreate({ commit }, reservation) {
+        return new Promise((resolve, reject) => {
+            axios.post("/api/reservation", reservation)
+                .then(response => {
+                    console.log(response);
+                    resolve(response.status == 200);
+                })
+                .catch(err => {
+                    console.warn(err);
+                    reject(err);
+                });
+        });
+    },
+    async ReservationUpdate({ commit }, reservation) {
+        return new Promise((resolve, reject) => {
+            axios.put("/api/reservation/" + reservation.id, reservation)
+                .then(response => {
+                    resolve(response.status == 200);
+                })
+                .catch(err => {
+                    console.warn(err);
+                    reject(err);
+                });
+        });
+    },
+    async ReservationDelete({ commit }, reservation) {
+        console.log(reservation);
+        return new Promise((resolve, reject) => {
+            axios.delete("/api/reservation/" + reservation.id)
+                .then(response => {
+                    resolve(response.status == 200);
+                })
+                .catch(err => {
+                    console.warn(err);
+                    reject(err);
+                });
+        });
+    },
+};
+
+
+export default {
+    actions,
+};
